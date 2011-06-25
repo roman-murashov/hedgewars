85c1eaaf6a51208b41052e6d03797518b5a19ddc
orange hg <orange.hg@gmail.com>
2011-06-25 15:10:08 +0000

    de nombreux petits changements : -quelques fautes d'accent -remplacement de mots que je trouvais mieux appropri?s -reformulation de phrases dont la traduction ?tait trop litt?raire.

---
+++ hedgewars/uSound.pas
@@ -0,0 +1,451 @@
+(*
+ * Hedgewars, a free turn based strategy game
+ * Copyright (c) 2004-2011 Andrey Korotaev <unC0Rr@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
+ *)
+
+{$INCLUDE "options.inc"}
+
+unit uSound;
+(*
+ * This unit controls the sounds and music of the game.
+ * Doesn't really do anything if isSoundEnabled = false.
+ *
+ * There are three basic types of sound controls:
+ *    Music        - The background music of the game:
+ *                   * will only be played if isMusicEnabled = true
+ *                   * can be started, changed, paused and resumed
+ *    Sound        - Can be started and stopped
+ *    Looped Sound - Subtype of sound: plays in a loop using a
+ *                   "channel", of which the id is returned on start.
+ *                   The channel id can be used to stop a specific sound loop.
+ *)
+interface
+uses SDLh, uConsts, uTypes, sysutils;
+
+var MusicFN: shortstring; // music file name
+
+procedure initModule;
+procedure freeModule;
+
+procedure InitSound; // Initiates sound-system if isSoundEnabled.
+procedure ReleaseSound; // Releases sound-system and used resources.
+procedure SoundLoad; // Preloads some sounds for performance reasons.
+
+
+// MUSIC
+
+// Obvious music commands for music track specified in MusicFN.
+procedure PlayMusic;
+procedure PauseMusic;
+procedure ResumeMusic;
+procedure ChangeMusic; // Replaces music track with current MusicFN and plays it.
+
+
+// SOUNDS
+
+// Plays the sound snd [from a given voicepack],
+// if keepPlaying is given and true,
+// then the sound's playback won't be interrupted if asked to play again.
+procedure PlaySound(snd: TSound);
+procedure PlaySound(snd: TSound; keepPlaying: boolean);
+procedure PlaySound(snd: TSound; voicepack: PVoicepack);
+procedure PlaySound(snd: TSound; voicepack: PVoicepack; keepPlaying: boolean);
+
+// Plays sound snd [of voicepack] in a loop, but starts with fadems milliseconds of fade-in.
+// Returns sound channel of the looped sound.
+function  LoopSound(snd: TSound): LongInt;
+function  LoopSound(snd: TSound; fadems: LongInt): LongInt;
+function  LoopSound(snd: TSound; voicepack: PVoicepack): LongInt; // WTF?
+function  LoopSound(snd: TSound; voicepack: PVoicepack; fadems: LongInt): LongInt;
+
+// Stops the normal/looped sound of the given type/in the given channel
+// [with a fade-out effect for fadems milliseconds].
+procedure StopSound(snd: TSound);
+procedure StopSound(chn: LongInt);
+procedure StopSound(chn, fadems: LongInt);
+
+
+// MISC
+
+// Modifies the sound volume of the game by voldelta and returns the new volume level.
+function  ChangeVolume(voldelta: LongInt): LongInt;
+
+// Returns a pointer to the voicepack with the given name.
+function  AskForVoicepack(name: shortstring): Pointer;
+
+
+implementation
+uses uVariables, uConsole, uUtils, uCommands, uDebug;
+
+const chanTPU = 32;
+var Volume: LongInt;
+    lastChan: array [TSound] of LongInt;
+    voicepacks: array[0..cMaxTeams] of TVoicepack;
+    defVoicepack: PVoicepack;
+    Mus: PMixMusic = nil;
+
+function  AskForVoicepack(name: shortstring): Pointer;
+var i: Longword;
+    locName, path: shortstring;
+begin
+i:= 0;
+    // First, attempt to locate a localised version of the voice
+    if cLocale <> 'en' then
+        begin
+        locName:= name+'_'+cLocale;
+        path:= UserPathz[ptVoices] + '/' + locName;
+        if DirectoryExists(path) then name:= locName
+        else
+            begin
+            path:= Pathz[ptVoices] + '/' + locName;
+            if DirectoryExists(path) then name:= locName
+            else if Length(cLocale) > 2 then
+                begin
+                locName:= name+'_'+Copy(cLocale,1,2);
+                path:= UserPathz[ptVoices] + '/' + locName;
+                if DirectoryExists(path) then name:= locName
+                else
+                    begin
+                    path:= Pathz[ptVoices] + '/' + locName;
+                    if DirectoryExists(path) then name:= locName
+                    end
+                end
+            end
+        end;
+
+    // If that fails, use the unmodified one
+    while (voicepacks[i].name <> name) and (voicepacks[i].name <> '') do
+        begin
+        inc(i);
+        TryDo(i <= cMaxTeams, 'Engine bug: AskForVoicepack i > cMaxTeams', true)
+        end;
+
+    voicepacks[i].name:= name;
+    AskForVoicepack:= @voicepacks[i]
+end;
+
+procedure InitSound;
+var i: TSound;
+    channels: LongInt;
+begin
+    if not isSoundEnabled then exit;
+    WriteToConsole('Init sound...');
+    isSoundEnabled:= SDL_InitSubSystem(SDL_INIT_AUDIO) >= 0;
+
+{$IFDEF IPHONEOS}
+    channels:= 1;
+{$ELSE}
+    channels:= 2;
+{$ENDIF}
+
+    if isSoundEnabled then
+        isSoundEnabled:= Mix_OpenAudio(44100, $8010, channels, 1024) = 0;
+
+{$IFDEF SDL_MIXER_NEWER}
+    WriteToConsole('Init SDL_mixer... ');
+    SDLTry(Mix_Init(MIX_INIT_OGG) <> 0, true);
+    WriteLnToConsole(msgOK);
+{$ENDIF}
+
+    if isSoundEnabled then
+        WriteLnToConsole(msgOK)
+    else
+        WriteLnToConsole(msgFailed);
+
+    Mix_AllocateChannels(Succ(chanTPU));
+    if isMusicEnabled then
+        Mix_VolumeMusic(50);
+    for i:= Low(TSound) to High(TSound) do
+        lastChan[i]:= -1;
+
+    Volume:= 0;
+    ChangeVolume(cInitVolume)
+end;
+
+procedure ReleaseSound;
+var i: TSound;
+    t: Longword;
+begin
+    for t:= 0 to cMaxTeams do
+        if voicepacks[t].name <> '' then
+            for i:= Low(TSound) to High(TSound) do
+                if voicepacks[t].chunks[i] <> nil then
+                    Mix_FreeChunk(voicepacks[t].chunks[i]);
+
+    if Mus <> nil then
+        Mix_FreeMusic(Mus);
+
+{$IFDEF SDL_MIXER_NEWER}
+    // make sure all instances of sdl_mixer are unloaded before continuing
+    while Mix_Init(0) <> 0 do
+        Mix_Quit();
+{$ENDIF}
+
+    Mix_CloseAudio();
+end;
+
+procedure SoundLoad;
+var i: TSound;
+    t: Longword;
+    s:shortstring;
+begin
+    if not isSoundEnabled then exit;
+
+    defVoicepack:= AskForVoicepack('Default');
+
+    for t:= 0 to cMaxTeams do
+        if voicepacks[t].name <> '' then
+            for i:= Low(TSound) to High(TSound) do
+                voicepacks[t].chunks[i]:= nil;
+
+    for i:= Low(TSound) to High(TSound) do
+    begin
+        defVoicepack^.chunks[i]:= nil;
+        // preload all the big sound files (>32k) that would otherwise lockup the game
+        if (i in [sndBeeWater, sndBee, sndCake, sndHellishImpact1, sndHellish, sndHomerun,
+                  sndMolotov, sndMortar, sndRideOfTheValkyries, sndYoohoo])
+            and (Soundz[i].Path <> ptVoices) and (Soundz[i].FileName <> '') then
+        begin
+            s:= UserPathz[Soundz[i].Path] + '/' + Soundz[i].FileName;
+            if not FileExists(s) then s:= Pathz[Soundz[i].Path] + '/' + Soundz[i].FileName;
+            WriteToConsole(msgLoading + s + ' ');
+            defVoicepack^.chunks[i]:= Mix_LoadWAV_RW(SDL_RWFromFile(Str2PChar(s), 'rb'), 1);
+            TryDo(defVoicepack^.chunks[i] <> nil, msgFailed, true);
+            WriteLnToConsole(msgOK);
+        end;
+    end;
+
+end;
+
+procedure PlaySound(snd: TSound);
+begin
+    PlaySound(snd, nil, false);
+end;
+
+procedure PlaySound(snd: TSound; keepPlaying: boolean);
+begin
+    PlaySound(snd, nil, keepPlaying);
+end;
+
+procedure PlaySound(snd: TSound; voicepack: PVoicepack);
+begin
+    PlaySound(snd, voicepack, false);
+end;
+
+procedure PlaySound(snd: TSound; voicepack: PVoicepack; keepPlaying: boolean);
+var s:shortstring;
+begin
+    if (not isSoundEnabled) or fastUntilLag then
+        exit;
+
+    if keepPlaying and (lastChan[snd] <> -1) and (Mix_Playing(lastChan[snd]) <> 0) then
+        exit;
+
+    if (voicepack <> nil) then
+    begin
+        if (voicepack^.chunks[snd] = nil) and (Soundz[snd].Path = ptVoices) and (Soundz[snd].FileName <> '') then
+        begin
+            s:= UserPathz[Soundz[snd].Path] + '/' + voicepack^.name + '/' + Soundz[snd].FileName;
+            if not FileExists(s) then s:= Pathz[Soundz[snd].Path] + '/' + voicepack^.name + '/' + Soundz[snd].FileName;
+            WriteToConsole(msgLoading + s + ' ');
+            voicepack^.chunks[snd]:= Mix_LoadWAV_RW(SDL_RWFromFile(Str2PChar(s), 'rb'), 1);
+            if voicepack^.chunks[snd] = nil then
+                WriteLnToConsole(msgFailed)
+            else
+                WriteLnToConsole(msgOK)
+        end;
+        lastChan[snd]:= Mix_PlayChannelTimed(-1, voicepack^.chunks[snd], 0, -1)
+    end
+    else
+    begin
+        if (defVoicepack^.chunks[snd] = nil) and (Soundz[snd].Path <> ptVoices) and (Soundz[snd].FileName <> '') then
+        begin
+            s:= UserPathz[Soundz[snd].Path] + '/' + Soundz[snd].FileName;
+            if not FileExists(s) then s:= Pathz[Soundz[snd].Path] + '/' + Soundz[snd].FileName;
+            WriteToConsole(msgLoading + s + ' ');
+            defVoicepack^.chunks[snd]:= Mix_LoadWAV_RW(SDL_RWFromFile(Str2PChar(s), 'rb'), 1);
+            TryDo(defVoicepack^.chunks[snd] <> nil, msgFailed, true);
+            WriteLnToConsole(msgOK);
+        end;
+        lastChan[snd]:= Mix_PlayChannelTimed(-1, defVoicepack^.chunks[snd], 0, -1)
+    end;
+end;
+
+function LoopSound(snd: TSound): LongInt;
+begin
+    LoopSound:= LoopSound(snd, nil)
+end;
+
+function LoopSound(snd: TSound; fadems: LongInt): LongInt;
+begin
+    LoopSound:= LoopSound(snd, nil, fadems)
+end;
+
+function LoopSound(snd: TSound; voicepack: PVoicepack): LongInt;
+begin
+    voicepack:= voicepack;    // avoid compiler hint
+    LoopSound:= LoopSound(snd, nil, 0)
+end;
+
+function LoopSound(snd: TSound; voicepack: PVoicepack; fadems: LongInt): LongInt;
+var s: shortstring;
+begin
+    if (not isSoundEnabled) or fastUntilLag then
+    begin
+        LoopSound:= -1;
+        exit
+    end;
+
+    if (voicepack <> nil) then
+    begin
+        if (voicepack^.chunks[snd] = nil) and (Soundz[snd].Path = ptVoices) and (Soundz[snd].FileName <> '') then
+        begin
+            s:= UserPathz[Soundz[snd].Path] + '/' + voicepack^.name + '/' + Soundz[snd].FileName;
+            if not FileExists(s) then s:= Pathz[Soundz[snd].Path] + '/' + voicepack^.name + '/' + Soundz[snd].FileName;
+            WriteToConsole(msgLoading + s + ' ');
+            voicepack^.chunks[snd]:= Mix_LoadWAV_RW(SDL_RWFromFile(Str2PChar(s), 'rb'), 1);
+            if voicepack^.chunks[snd] = nil then
+                WriteLnToConsole(msgFailed)
+            else
+                WriteLnToConsole(msgOK)
+        end;
+        LoopSound:= Mix_PlayChannelTimed(-1, voicepack^.chunks[snd], -1, -1)
+    end
+    else
+    begin
+        if (defVoicepack^.chunks[snd] = nil) and (Soundz[snd].Path <> ptVoices) and (Soundz[snd].FileName <> '') then
+        begin
+            s:= UserPathz[Soundz[snd].Path] + '/' + Soundz[snd].FileName;
+            if not FileExists(s) then s:= Pathz[Soundz[snd].Path] + '/' + Soundz[snd].FileName;
+            WriteToConsole(msgLoading + s + ' ');
+            defVoicepack^.chunks[snd]:= Mix_LoadWAV_RW(SDL_RWFromFile(Str2PChar(s), 'rb'), 1);
+            TryDo(defVoicepack^.chunks[snd] <> nil, msgFailed, true);
+            WriteLnToConsole(msgOK);
+        end;
+        if fadems > 0 then
+            LoopSound:= Mix_FadeInChannelTimed(-1, defVoicepack^.chunks[snd], -1, fadems, -1)
+        else
+            LoopSound:= Mix_PlayChannelTimed(-1, defVoicepack^.chunks[snd], -1, -1);
+    end;
+end;
+
+procedure StopSound(snd: TSound);
+begin
+    if not isSoundEnabled then exit;
+
+    if (lastChan[snd] <> -1) and (Mix_Playing(lastChan[snd]) <> 0) then
+    begin
+        Mix_HaltChannel(lastChan[snd]);
+        lastChan[snd]:= -1;
+    end;
+end;
+
+procedure StopSound(chn: LongInt);
+begin
+    if not isSoundEnabled then exit;
+
+    if (chn <> -1) and (Mix_Playing(chn) <> 0) then
+        Mix_HaltChannel(chn);
+end;
+
+procedure StopSound(chn, fadems: LongInt);
+begin
+    if not isSoundEnabled then exit;
+
+    if (chn <> -1) and (Mix_Playing(chn) <> 0) then
+        Mix_FadeOutChannel(chn, fadems);
+end;
+
+procedure PlayMusic;
+var s: shortstring;
+begin
+    if (not isSoundEnabled) or (MusicFN = '') or (not isMusicEnabled) then
+        exit;
+
+    s:= UserPathPrefix + '/Data/Music/' + MusicFN;
+    if not FileExists(s) then s:= PathPrefix + '/Music/' + MusicFN;
+    WriteToConsole(msgLoading + s + ' ');
+
+    Mus:= Mix_LoadMUS(Str2PChar(s));
+    TryDo(Mus <> nil, msgFailed, false);
+    WriteLnToConsole(msgOK);
+
+    SDLTry(Mix_FadeInMusic(Mus, -1, 3000) <> -1, false)
+end;
+
+function ChangeVolume(voldelta: LongInt): LongInt;
+begin
+    if not isSoundEnabled then
+        exit(0);
+
+    inc(Volume, voldelta);
+    if Volume < 0 then Volume:= 0;
+    Mix_Volume(-1, Volume);
+    Volume:= Mix_Volume(-1, -1);
+    if isMusicEnabled then Mix_VolumeMusic(Volume * 4 div 8);
+    ChangeVolume:= Volume * 100 div MIX_MAX_VOLUME
+end;
+
+procedure PauseMusic;
+begin
+    if (MusicFN = '') or (not isMusicEnabled) then
+        exit;
+
+    Mix_PauseMusic(Mus);
+end;
+
+procedure ResumeMusic;
+begin
+    if (MusicFN = '') or (not isMusicEnabled) then
+        exit;
+
+    Mix_ResumeMusic(Mus);
+end;
+
+procedure ChangeMusic;
+begin
+    if (MusicFN = '') or (not isMusicEnabled) then
+        exit;
+
+    // get rid of current music
+    if Mus <> nil then
+        Mix_FreeMusic(Mus);
+
+    PlayMusic;
+end;
+
+procedure chVoicepack(var s: shortstring);
+begin
+    if CurrentTeam = nil then OutError(errmsgIncorrectUse + ' "/voicepack"', true);
+    if s[1]='"' then Delete(s, 1, 1);
+    if s[byte(s[0])]='"' then Delete(s, byte(s[0]), 1);
+    CurrentTeam^.voicepack:= AskForVoicepack(s)
+end;
+
+procedure initModule;
+begin
+    RegisterVariable('voicepack', vtCommand, @chVoicepack, false);
+    MusicFN:='';
+end;
+
+procedure freeModule;
+begin
+    if isSoundEnabled then
+        ReleaseSound();
+end;
+
+end.
+
---
+++ project_files/HedgewarsMobile/Classes/CreationChamber.m
@@ -0,0 +1,192 @@
+/*
+ * Hedgewars-iOS, a Hedgewars port for iOS devices
+ * Copyright (c) 2009-2011 Vittorio Giovara <vittorio.giovara@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * File created on 12/11/2010.
+ */
+
+
+#import "CreationChamber.h"
+#import "hwconsts.h"
+
+void createSettings () {
+    NSUserDefaults *settings = [NSUserDefaults standardUserDefaults];
+    [settings setObject:[NSNumber numberWithBool:NO] forKey:@"alternate"];
+    [settings setObject:[NSNumber numberWithBool:YES] forKey:@"music"];
+    [settings setObject:[NSNumber numberWithBool:YES] forKey:@"sound"];
+    [settings setObject:[NSNumber numberWithBool:NO] forKey:@"classic_menu"];
+    [settings setObject:[NSNumber numberWithBool:YES] forKey:@"enhanced"];
+    [settings setObject:[NSNumber numberWithBool:YES] forKey:@"multitasking"];
+
+    // don't overwrite these two strings when present
+    if ([settings objectForKey:@"username"] == nil)
+        [settings setObject:@"" forKey:@"username"];
+    if ([settings objectForKey:@"password"] == nil)
+        [settings setObject:@"" forKey:@"password"];
+
+    [settings synchronize];
+}
+
+void createTeamNamed (NSString *nameWithoutExt) {
+    NSString *teamsDirectory = TEAMS_DIRECTORY();
+
+    if (![[NSFileManager defaultManager] fileExistsAtPath: teamsDirectory]) {
+        [[NSFileManager defaultManager] createDirectoryAtPath:teamsDirectory
+                                  withIntermediateDirectories:NO
+                                                   attributes:nil
+                                                        error:NULL];
+    }
+
+    NSMutableArray *hedgehogs = [[NSMutableArray alloc] initWithCapacity: HW_getMaxNumberOfHogs()];
+
+    for (int i = 0; i < HW_getMaxNumberOfHogs(); i++) {
+        NSString *hogName = [[NSString alloc] initWithFormat:@"hedgehog %d",i];
+        NSDictionary *hog = [[NSDictionary alloc] initWithObjectsAndKeys:
+                             [NSNumber numberWithInt:0],@"level",
+                             hogName,@"hogname",
+                             @"NoHat",@"hat",
+                             nil];
+        [hogName release];
+        [hedgehogs addObject:hog];
+        [hog release];
+    }
+
+    NSDictionary *theTeam = [[NSDictionary alloc] initWithObjectsAndKeys:
+                             @"0",@"hash",
+                             @"Statue",@"grave",
+                             @"Plane",@"fort",
+                             @"Default",@"voicepack",
+                             @"hedgewars",@"flag",
+                             hedgehogs,@"hedgehogs",
+                             nil];
+    [hedgehogs release];
+
+    NSString *teamFile = [[NSString alloc] initWithFormat:@"%@/%@.plist", teamsDirectory, nameWithoutExt];
+
+    [theTeam writeToFile:teamFile atomically:YES];
+    [teamFile release];
+    [theTeam release];
+}
+
+void createWeaponNamed (NSString *nameWithoutExt, int type) {
+    NSString *weaponsDirectory = WEAPONS_DIRECTORY();
+
+    if (![[NSFileManager defaultManager] fileExistsAtPath: weaponsDirectory]) {
+        [[NSFileManager defaultManager] createDirectoryAtPath:weaponsDirectory
+                                  withIntermediateDirectories:NO
+                                                   attributes:nil
+                                                        error:NULL];
+    }
+
+    NSInteger ammolineSize = HW_getNumberOfWeapons();
+    NSString *qt, *prob, *delay, *crate;
+    switch (type) {
+        default: //default
+            qt = [[NSString alloc] initWithBytes:AMMOLINE_DEFAULT_QT length:ammolineSize encoding:NSUTF8StringEncoding];
+            prob = [[NSString alloc] initWithBytes:AMMOLINE_DEFAULT_QT length:ammolineSize encoding:NSUTF8StringEncoding];
+            delay = [[NSString alloc] initWithBytes:AMMOLINE_DEFAULT_QT length:ammolineSize encoding:NSUTF8StringEncoding];
+            crate = [[NSString alloc] initWithBytes:AMMOLINE_DEFAULT_QT length:ammolineSize encoding:NSUTF8StringEncoding];
+            break;
+        case 1: //crazy
+            qt = [[NSString alloc] initWithBytes:AMMOLINE_CRAZY_QT length:ammolineSize encoding:NSUTF8StringEncoding];
+            prob = [[NSString alloc] initWithBytes:AMMOLINE_CRAZY_PROB length:ammolineSize encoding:NSUTF8StringEncoding];
+            delay = [[NSString alloc] initWithBytes:AMMOLINE_CRAZY_DELAY length:ammolineSize encoding:NSUTF8StringEncoding];
+            crate = [[NSString alloc] initWithBytes:AMMOLINE_CRAZY_CRATE length:ammolineSize encoding:NSUTF8StringEncoding];
+            break;
+        case 2: //pro mode
+            qt = [[NSString alloc] initWithBytes:AMMOLINE_PROMODE_QT length:ammolineSize encoding:NSUTF8StringEncoding];
+            prob = [[NSString alloc] initWithBytes:AMMOLINE_PROMODE_PROB length:ammolineSize encoding:NSUTF8StringEncoding];
+            delay = [[NSString alloc] initWithBytes:AMMOLINE_PROMODE_DELAY length:ammolineSize encoding:NSUTF8StringEncoding];
+            crate = [[NSString alloc] initWithBytes:AMMOLINE_PROMODE_CRATE length:ammolineSize encoding:NSUTF8StringEncoding];
+            break;
+        case 3: //shoppa
+            qt = [[NSString alloc] initWithBytes:AMMOLINE_SHOPPA_QT length:ammolineSize encoding:NSUTF8StringEncoding];
+            prob = [[NSString alloc] initWithBytes:AMMOLINE_SHOPPA_PROB length:ammolineSize encoding:NSUTF8StringEncoding];
+            delay = [[NSString alloc] initWithBytes:AMMOLINE_SHOPPA_DELAY length:ammolineSize encoding:NSUTF8StringEncoding];
+            crate = [[NSString alloc] initWithBytes:AMMOLINE_SHOPPA_CRATE length:ammolineSize encoding:NSUTF8StringEncoding];
+            break;
+        case 4: //clean slate
+            qt = [[NSString alloc] initWithBytes:AMMOLINE_CLEAN_QT length:ammolineSize encoding:NSUTF8StringEncoding];
+            prob = [[NSString alloc] initWithBytes:AMMOLINE_CLEAN_PROB length:ammolineSize encoding:NSUTF8StringEncoding];
+            delay = [[NSString alloc] initWithBytes:AMMOLINE_CLEAN_DELAY length:ammolineSize encoding:NSUTF8StringEncoding];
+            crate = [[NSString alloc] initWithBytes:AMMOLINE_CLEAN_CRATE length:ammolineSize encoding:NSUTF8StringEncoding];
+            break;
+        case 5: //minefield
+            qt = [[NSString alloc] initWithBytes:AMMOLINE_MINES_QT length:ammolineSize encoding:NSUTF8StringEncoding];
+            prob = [[NSString alloc] initWithBytes:AMMOLINE_MINES_PROB length:ammolineSize encoding:NSUTF8StringEncoding];
+            delay = [[NSString alloc] initWithBytes:AMMOLINE_MINES_DELAY length:ammolineSize encoding:NSUTF8StringEncoding];
+            crate = [[NSString alloc] initWithBytes:AMMOLINE_MINES_CRATE length:ammolineSize encoding:NSUTF8StringEncoding];
+            break;
+        case 6: //thinking with portals
+            qt = [[NSString alloc] initWithBytes:AMMOLINE_PORTALS_QT length:ammolineSize encoding:NSUTF8StringEncoding];
+            prob = [[NSString alloc] initWithBytes:AMMOLINE_PORTALS_PROB length:ammolineSize encoding:NSUTF8StringEncoding];
+            delay = [[NSString alloc] initWithBytes:AMMOLINE_PORTALS_DELAY length:ammolineSize encoding:NSUTF8StringEncoding];
+            crate = [[NSString alloc] initWithBytes:AMMOLINE_PORTALS_CRATE length:ammolineSize encoding:NSUTF8StringEncoding];
+            break;
+    }
+
+    NSDictionary *theWeapon = [[NSDictionary alloc] initWithObjectsAndKeys: qt,@"ammostore_initialqt",
+                               prob,@"ammostore_probability", delay,@"ammostore_delay", crate,@"ammostore_crate", nil];
+    [qt release];
+    [prob release];
+    [delay release];
+    [crate release];
+
+    NSString *weaponFile = [[NSString alloc] initWithFormat:@"%@/%@.plist", weaponsDirectory, nameWithoutExt];
+    [theWeapon writeToFile:weaponFile atomically:YES];
+    [weaponFile release];
+    [theWeapon release];
+}
+
+void createSchemeNamed (NSString *nameWithoutExt) {
+    NSString *schemesDirectory = SCHEMES_DIRECTORY();
+
+    if (![[NSFileManager defaultManager] fileExistsAtPath: schemesDirectory]) {
+        [[NSFileManager defaultManager] createDirectoryAtPath:schemesDirectory
+                                  withIntermediateDirectories:NO
+                                                   attributes:nil
+                                                        error:NULL];
+    }
+
+    // load data to get the size of the arrays and their default values
+    NSArray *basicSettings = [[NSArray alloc] initWithContentsOfFile:BASICFLAGS_FILE()];
+    NSMutableArray *basicArray  = [[NSMutableArray alloc] initWithCapacity:[basicSettings count]];
+    for (NSDictionary *basicDict in basicSettings)
+        [basicArray addObject:[basicDict objectForKey:@"default"]];
+    [basicSettings release];
+
+    NSArray *mods = [[NSArray alloc] initWithContentsOfFile:GAMEMODS_FILE()];
+    NSMutableArray *gamemodArray= [[NSMutableArray alloc] initWithCapacity:[mods count]];
+    for (int i = 0; i < [mods count]; i++)
+        [gamemodArray addObject:[NSNumber numberWithBool:NO]];
+    [mods release];
+
+    // workaround for randomorder that has to be set to YES
+    [gamemodArray replaceObjectAtIndex:11 withObject:[NSNumber numberWithBool:YES]];
+
+    NSMutableDictionary *theScheme = [[NSMutableDictionary alloc] initWithObjectsAndKeys:
+                                      basicArray,@"basic",
+                                      gamemodArray,@"gamemod",
+                                      nil];
+    [gamemodArray release];
+    [basicArray release];
+    
+    NSString *schemeFile = [[NSString alloc] initWithFormat:@"%@/%@.plist", schemesDirectory, nameWithoutExt];
+    
+    [theScheme writeToFile:schemeFile atomically:YES];
+    [schemeFile release];
+    [theScheme release];
+}
