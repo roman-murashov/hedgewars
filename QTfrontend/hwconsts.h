c485a53739e10dcab9eed436e60d2978d50ab058
unc0rr <unc0rr@localhost>
2011-06-25 16:39:54 +0400

    Fix camera jerking on zoom in

---
+++ QTfrontend/hwconsts.h
@@ -0,0 +1,111 @@
+/*
+ * Hedgewars, a free turn based strategy game
+ * Copyright (c) 2005-2011 Andrey Korotaev <unC0Rr@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
+ */
+
+#if !defined(TARGET_OS_IPHONE)
+#include <QString>
+#include <QDir>
+#include <QStringList>
+#include <QPair>
+
+#include "themesmodel.h"
+
+extern QString * cProtoVer;
+extern QString * cVersionString;
+extern QString * cDataDir;
+extern QString * cConfigDir;
+
+extern QDir * bindir;
+extern QDir * cfgdir;
+extern QDir * datadir;
+
+extern bool custom_config;
+extern bool custom_data;
+
+extern int cMaxTeams;
+extern int cMinServerVersion;
+
+class QStringListModel;
+
+extern ThemesModel * themesModel;
+extern QStringList * mapList;
+extern QStringList * scriptList;
+
+extern QString * cDefaultAmmoStore;
+extern int cAmmoNumber;
+extern QList< QPair<QString, QString> > cDefaultAmmos;
+
+extern unsigned int colors[];
+
+extern QString * netHost;
+extern quint16 netPort;
+
+extern bool haveServer;
+extern bool isDevBuild;
+#endif
+
+#define AMMOLINE_DEFAULT_QT     "939192942219912103223511100120100000021111010101111991"
+#define AMMOLINE_DEFAULT_PROB   "040504054160065554655446477657666666615551010111541111"
+#define AMMOLINE_DEFAULT_DELAY  "000000000000020550000004000700400000000022000000060000"
+#define AMMOLINE_DEFAULT_CRATE  "131111031211111112311411111111111111121111110111111111"
+
+//TODO: Remove Piano's unlimited uses!
+#define AMMOLINE_CRAZY_QT       "999999999999999999299999999999999929999999990999999229"
+#define AMMOLINE_CRAZY_PROB     "111111011111111111111111111111111111111111110111111111"
+#define AMMOLINE_CRAZY_DELAY    "000000000000000000000000000000000000000000000000000000"
+#define AMMOLINE_CRAZY_CRATE    "131111031211111112311411111111111111121111010111111111"
+
+#define AMMOLINE_PROMODE_QT     "909000900000000000000900000000000000000000000000000000"
+#define AMMOLINE_PROMODE_PROB   "000000000000000000000000000000000000000000000000000000"
+#define AMMOLINE_PROMODE_DELAY  "000000000000020550000004000700400000000020000000000000"
+#define AMMOLINE_PROMODE_CRATE  "111111111111111111111111111111111111111110010111111111"
+
+#define AMMOLINE_SHOPPA_QT      "000000990000000000000000000000000000000000000000000000"
+#define AMMOLINE_SHOPPA_PROB    "444441004424440221011212122242200000000200040001001111"
+#define AMMOLINE_SHOPPA_DELAY   "000000000000000000000000000000000000000000000000000000"
+#define AMMOLINE_SHOPPA_CRATE   "111111111111111111111111111111111111111110110111111111"
+
+#define AMMOLINE_CLEAN_QT       "101000900001000001100000000000000000000000000000100000"
+#define AMMOLINE_CLEAN_PROB     "040504054160065554655446477657666666615551010111541111"
+#define AMMOLINE_CLEAN_DELAY    "000000000000000000000000000000000000000000000000000000"
+#define AMMOLINE_CLEAN_CRATE    "131111031211111112311411111111111111121111110111111111"
+
+#define AMMOLINE_MINES_QT       "000000990009000000030000000000000000000000000000000000"
+#define AMMOLINE_MINES_PROB     "000000000000000000000000000000000000000000000000000000"
+#define AMMOLINE_MINES_DELAY    "000000000000020550000004000700400000000020000000060000"
+#define AMMOLINE_MINES_CRATE    "111111111111111111111111111111111111111111110111111111"
+
+#define AMMOLINE_PORTALS_QT     "900000900200000000210000000000000011000009000000000000"
+#define AMMOLINE_PORTALS_PROB   "040504054160065554655446477657666666615551010111541111"
+#define AMMOLINE_PORTALS_DELAY  "000000000000020550000004000700400000000020000000060000"
+#define AMMOLINE_PORTALS_CRATE  "131111031211111112311411111111111111121111110111111111"
+
+
+#define NETGAME_DEFAULT_PORT 46631
+
+
+// see http://en.wikipedia.org/wiki/List_of_colors
+#define HW_TEAMCOLOR_ARRAY  { 0xff007fff, /* azure */ \
+                              0xffdd0000, /* classic red */ \
+                              0xff3e9321, /* classic green */ \
+                              0xffa23dbb, /* classic purple */ \
+                              0xffffb347, /* pastel orange */ \
+                              0xffcfcfc4, /* pastel gray */ \
+                              0xffbff000, /* lime */ \
+                              0xffffef00, /* yellow */ \
+                              /* add new colors here */ \
+                              0 }
---
+++ project_files/HedgewarsMobile/Classes/ObjcExports.m
@@ -0,0 +1,143 @@
+/*
+ * Hedgewars-iOS, a Hedgewars port for iOS devices
+ * Copyright (c) 2009-2011 Vittorio Giovara <vittorio.giovara@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * File created on 30/10/2010.
+ */
+
+
+#import "ObjcExports.h"
+#import "OverlayViewController.h"
+#import "AmmoMenuViewController.h"
+
+#pragma mark -
+#pragma mark internal variables
+// actual game started (controls should be enabled)
+BOOL gameRunning;
+// black screen present
+BOOL savedGame;
+// cache the grenade time
+NSInteger grenadeTime;
+// the reference to the newMenu instance
+OverlayViewController *overlay_instance;
+
+
+#pragma mark -
+#pragma mark functions called like oop
+void objcExportsInit(OverlayViewController* instance) {
+    overlay_instance = instance;
+    gameRunning = NO;
+    savedGame = NO;
+    grenadeTime = 2;
+}
+
+BOOL inline isGameRunning() {
+    return gameRunning;
+}
+
+void inline setGameRunning(BOOL value) {
+    gameRunning = value;
+}
+
+NSInteger cachedGrenadeTime() {
+    return grenadeTime;
+}
+
+void inline setGrenadeTime(NSInteger value) {
+    grenadeTime = value;
+}
+
+#pragma mark -
+#pragma mark functions called by pascal code
+void startSpinningProgress() {
+    gameRunning = NO;
+    overlay_instance.lowerIndicator = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhiteLarge];
+
+    CGPoint center = overlay_instance.view.center;
+    overlay_instance.lowerIndicator.center = (IS_DUALHEAD() ? CGPointMake(center.y, center.x)
+                                              : CGPointMake(center.y, center.x * 5/3));
+
+    [overlay_instance.lowerIndicator startAnimating];
+    [overlay_instance.view addSubview:overlay_instance.lowerIndicator];
+    [overlay_instance.lowerIndicator release];
+}
+
+void stopSpinningProgress() {
+    [overlay_instance.lowerIndicator stopAnimating];
+    [overlay_instance.lowerIndicator removeFromSuperview];
+    HW_zoomSet(1.7);
+    if (savedGame == NO)
+        gameRunning = YES;
+}
+
+void clearView() {
+    // don't use any engine calls here as this function is called every time the ammomenu is opened
+    [UIView beginAnimations:@"remove button" context:NULL];
+    [UIView setAnimationDuration:ANIMATION_DURATION];
+    overlay_instance.confirmButton.alpha = 0;
+    overlay_instance.grenadeTimeSegment.alpha = 0;
+    [UIView commitAnimations];
+
+    if (overlay_instance.confirmButton)
+        [overlay_instance.confirmButton performSelector:@selector(removeFromSuperview) withObject:nil afterDelay:ANIMATION_DURATION];
+    if (overlay_instance.grenadeTimeSegment) {
+        [overlay_instance.grenadeTimeSegment performSelector:@selector(removeFromSuperview) withObject:nil afterDelay:ANIMATION_DURATION];
+        overlay_instance.grenadeTimeSegment.tag = 0;
+    }
+    grenadeTime = 2;
+}
+
+void saveBeganSynching() {
+    savedGame = YES;
+    stopSpinningProgress();
+
+    overlay_instance.view.backgroundColor = [UIColor blackColor];
+    overlay_instance.view.alpha = 0.75;
+    overlay_instance.view.userInteractionEnabled = NO;
+
+    overlay_instance.savesIndicator = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhiteLarge];
+
+    CGPoint center = overlay_instance.view.center;
+    overlay_instance.savesIndicator.center = CGPointMake(center.y, center.x);
+    overlay_instance.savesIndicator.hidesWhenStopped = YES;
+
+    [overlay_instance.savesIndicator startAnimating];
+    [overlay_instance.view addSubview:overlay_instance.savesIndicator];
+    [overlay_instance.savesIndicator release];
+}
+
+void saveFinishedSynching() {
+    [UIView beginAnimations:@"fading from save synch" context:NULL];
+    [UIView setAnimationDuration:1];
+    overlay_instance.view.backgroundColor = [UIColor clearColor];
+    overlay_instance.view.alpha = 1;
+    overlay_instance.view.userInteractionEnabled = YES;
+    [UIView commitAnimations];
+
+    [overlay_instance.savesIndicator stopAnimating];
+    [overlay_instance.savesIndicator performSelector:@selector(removeFromSuperview) withObject:nil afterDelay:1];
+
+    gameRunning = YES;
+}
+
+void updateVisualsNewTurn(void) {
+    [overlay_instance.amvc updateAmmoVisuals];
+}
+
+// dummy function to prevent linkage fail
+int SDL_main(int argc, char **argv) {
+    return 0;
+}
