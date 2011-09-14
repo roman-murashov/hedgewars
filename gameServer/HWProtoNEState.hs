fe9303e0c56c1437689805eb0a32f58963182238
sheepluva <sheepluva@localhost>
2011-09-14 19:07:18 +0200

    add blink to cyclops hat

---
+++ gameServer/HWProtoNEState.hs
@@ -0,0 +1,51 @@
+{-# LANGUAGE OverloadedStrings #-}
+module HWProtoNEState where
+
+import Control.Monad.Reader
+import qualified Data.ByteString.Char8 as B
+--------------------------------------
+import CoreTypes
+import Actions
+import Utils
+import RoomsAndClients
+
+handleCmd_NotEntered :: CmdHandler
+
+handleCmd_NotEntered ["NICK", newNick] = do
+    (ci, irnc) <- ask
+    let cl = irnc `client` ci
+    if not . B.null $ nick cl then return [ProtocolError "Nickname already chosen"]
+        else
+        if illegalName newNick then return [ByeClient "Illegal nickname"]
+            else
+            return $
+                ModifyClient (\c -> c{nick = newNick}) :
+                AnswerClients [sendChan cl] ["NICK", newNick] :
+                [CheckRegistered | clientProto cl /= 0]
+
+handleCmd_NotEntered ["PROTO", protoNum] = do
+    (ci, irnc) <- ask
+    let cl = irnc `client` ci
+    if clientProto cl > 0 then return [ProtocolError "Protocol already known"]
+        else
+        if parsedProto == 0 then return [ProtocolError "Bad number"]
+            else
+            return $
+                ModifyClient (\c -> c{clientProto = parsedProto}) :
+                AnswerClients [sendChan cl] ["PROTO", showB parsedProto] :
+                [CheckRegistered | not . B.null $ nick cl]
+    where
+        parsedProto = readInt_ protoNum
+
+
+handleCmd_NotEntered ["PASSWORD", passwd] = do
+    (ci, irnc) <- ask
+    let cl = irnc `client` ci
+
+    if passwd == webPassword cl then
+        return $ JoinLobby : [AnswerClients [sendChan cl] ["ADMIN_ACCESS"] | isAdministrator cl]
+        else
+        return [ByeClient "Authentication failed"]
+
+
+handleCmd_NotEntered _ = return [ProtocolError "Incorrect command (state: not entered)"]
