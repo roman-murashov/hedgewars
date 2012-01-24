48b0832a18661f59282439b246087bda11deb2a8
Xeli <Xeli@localhost>
2012-01-24 18:11:56 +0100

    added TARDIS to minimal data dir, changed a couple of org_libsdl_app to org_hedgewars_hedgeroid in SDL_android.cpp and updated SDLActivity.java

---
+++ hedgewars/pas2cSystem.pas
@@ -0,0 +1,41 @@
+system;
+
+type 
+    LongInt = integer;
+    LongWord = integer;
+    Cardinal = integer;
+    PtrInt = integer;
+    Word = integer;
+    Integer = integer;
+    Byte = integer;
+    SmallInt = integer;
+    ShortInt = integer;
+
+    pointer = pointer;
+    PChar = pointer;
+
+    double = float;
+    real = float;
+    float = float;
+
+    boolean = boolean;
+    LongBool = boolean;
+
+    string = string;
+    shortstring = string;
+    ansistring = string;
+
+    char = char;
+    
+    PByte = ^Byte;
+    PLongInt = ^LongInt;
+    PLongWord = ^LongWord;
+    PInteger = ^Integer;
+var 
+    false, true: boolean;
+    write, writeLn, read, readLn, inc, dec: procedure;
+    StrLen, ord, Succ, Pred : function : integer;
+    Low, High : function : integer;
+    Now : function : integer;
+    StrPas, FormatDateTime : function : shortstring;
+    exit : procedure;
