c838dec6a9cd1aa7803746df2ad4ca0221488d7b
koda <koda@localhost>
2013-02-20 01:55:23 +0100

    fix compiling on osx when you have multiple linkers installed

---
+++ QTfrontend/hwform.h
@@ -0,0 +1,205 @@
+/*
+ * Hedgewars, a free turn based strategy game
+ * Copyright (c) 2004-2012 Andrey Korotaev <unC0Rr@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
+ */
+
+#ifndef HWFORM_H
+#define HWFORM_H
+
+#include <QMainWindow>
+#include <QStack>
+#include <QTime>
+#include <QPointer>
+#include <QPropertyAnimation>
+#include <QUrl>
+#include <QNetworkReply>
+#include <QNetworkRequest>
+#include <QNetworkAccessManager>
+
+#include "netserver.h"
+#include "game.h"
+#include "ui_hwform.h"
+#include "SDLInteraction.h"
+#include "bgwidget.h"
+
+#ifdef __APPLE__
+#include "InstallController.h"
+#endif
+
+class HWGame;
+class HWTeam;
+class HWNamegen;
+class HWNewNet;
+class GameUIConfig;
+class HWNetRegisterServer;
+class QCloseEvent;
+class AmmoSchemeModel;
+class QSettings;
+class QSignalMapper;
+
+extern bool frontendEffects;
+extern QString playerHash;
+
+class HWForm : public QMainWindow
+{
+        Q_OBJECT
+
+    public:
+        HWForm(QWidget *parent = 0, QString styleSheet = "");
+        Ui_HWForm ui;
+        static GameUIConfig * config;
+        void updateXfire();
+        void PlayDemoQuick(const QString & demofilename);
+        void exit();
+        void setButtonDescription(QString desc);
+        void backDescription();
+        void GoToVideos();
+
+    private slots:
+        void GoToSaves();
+        void GoToDemos();
+        void GoToNet();
+        void GoToSelectWeapon();
+        void GoToSelectWeaponSet(int index);
+        void GoToSelectNewWeapon();
+        void GoToScheme(int index);
+        void GoToEditScheme();
+        void GoToNewScheme();
+        void GoToPage(int id);
+        void GoBack();
+        void OpenSnapshotFolder();
+        QString getDemoArguments();
+        void AssociateFiles();
+        void btnExitPressed();
+        void IntermediateSetup();
+        void NewTeam();
+        void EditTeam(const QString & teamName);
+        void AfterTeamEdit();
+        void DeleteTeam(const QString & teamName);
+        void DeleteScheme();
+        void DeleteWeaponSet();
+        void SimpleGame();
+        void PlayDemo();
+        void startTraining(const QString&);
+        void StartCampaign();
+        void NetConnect();
+        void NetConnectServer(const QString & host, quint16 port);
+        void NetConnectOfficialServer();
+        void NetStartServer();
+        void NetDisconnect();
+        void NetConnected();
+        void NetError(const QString & errmsg);
+        void NetWarning(const QString & wrnmsg);
+        void NetGameEnter();
+        void NetPassword(const QString & nick);
+        void NetNickRegistered(const QString & nick);
+        void NetNickNotRegistered(const QString & nick);
+        void NetNickTaken(const QString & nick);
+        void NetAuthFailed();
+        bool RetryDialog(const QString & title, const QString & label);
+        void NetTeamAccepted(const QString& team);
+        void AddNetTeam(const HWTeam& team);
+        void RemoveNetTeam(const HWTeam& team);
+        void StartMPGame();
+        void GameStateChanged(GameState gameState);
+        void ForcedDisconnect(const QString & reason);
+        void ShowErrorMessage(const QString &);
+        void GetRecord(RecordType type, const QByteArray & record);
+        void CreateNetGame();
+        void UpdateWeapons();
+        void onFrontendFullscreen(bool value);
+        void onFrontendEffects(bool value);
+        void Music(bool checked);
+        void UpdateCampaignPage(int index);
+        void UpdateCampaignPageProgress(int index);
+        void InitCampaignPage();
+        void showFeedbackDialog();
+
+        void NetGameChangeStatus(bool isMaster);
+        void NetGameMaster();
+        void NetGameSlave();
+
+        void AsyncNetServerStart();
+        void NetLeftRoom(const QString & reason);
+        void selectFirstNetScheme();
+
+        void saveDemoWithCustomName();
+        void openRegistrationPage();
+
+        void startGame();
+
+    private:
+        void _NetConnect(const QString & hostName, quint16 port, QString nick);
+        void UpdateTeamsLists();
+        void CreateGame(GameCFGWidget * gamecfg, TeamSelWidget* pTeamSelWidget, QString ammo);
+        void closeEvent(QCloseEvent *event);
+        void CustomizePalettes();
+        void resizeEvent(QResizeEvent * event);
+        QString stringifyPageId(quint32 id);
+        //void keyReleaseEvent(QKeyEvent *event);
+
+        enum PageIDs
+        {
+            ID_PAGE_SETUP_TEAM     ,
+            ID_PAGE_SETUP          ,
+            ID_PAGE_MULTIPLAYER    ,
+            ID_PAGE_DEMOS          ,
+            ID_PAGE_NET            ,
+            ID_PAGE_NETGAME        ,
+            ID_PAGE_INFO           ,
+            ID_PAGE_MAIN           ,
+            ID_PAGE_GAMESTATS      ,
+            ID_PAGE_SINGLEPLAYER   ,
+            ID_PAGE_TRAINING       ,
+            ID_PAGE_SELECTWEAPON   ,
+            ID_PAGE_NETSERVER      ,
+            ID_PAGE_INGAME         ,
+            ID_PAGE_ROOMSLIST      ,
+            ID_PAGE_CONNECTING     ,
+            ID_PAGE_SCHEME         ,
+            ID_PAGE_ADMIN          ,
+            ID_PAGE_CAMPAIGN       ,
+            ID_PAGE_DRAWMAP        ,
+            ID_PAGE_DATADOWNLOAD   ,
+            ID_PAGE_VIDEOS         ,
+            MAX_PAGE
+        };
+        QPointer<HWGame> game;
+        QPointer<HWNetServer> pnetserver;
+        QPointer<HWNetRegisterServer> pRegisterServer;
+        QPointer<HWTeam> editedTeam;
+        QPointer<HWNewNet> hwnet;
+        HWNamegen * namegen;
+        AmmoSchemeModel * ammoSchemeModel;
+        QStack<int> PagesStack;
+        QTime eggTimer;
+        BGWidget * wBackground;
+        QSignalMapper * pageSwitchMapper;
+        QByteArray m_lastDemo;
+
+        QPropertyAnimation *animationNewSlide;
+        QPropertyAnimation *animationOldSlide;
+        QPropertyAnimation *animationNewOpacity;
+        QPropertyAnimation *animationOldOpacity;
+
+#ifdef __APPLE__
+        InstallController * panel;
+#endif
+
+        void OnPageShown(quint8 id, quint8 lastid=0);
+};
+
+#endif
---
+++ QTfrontend/ui/widget/chatwidget.h
@@ -0,0 +1,145 @@
+/*
+ * Hedgewars, a free turn based strategy game
+ * Copyright (c) 2007 Igor Ulyanov <iulyanov@gmail.com>
+ * Copyright (c) 2004-2012 Andrey Korotaev <unC0Rr@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
+ */
+
+#ifndef _CHAT_WIDGET_INCLUDED
+#define _CHAT_WIDGET_INCLUDED
+
+#include <QWidget>
+#include <QString>
+#include <QGridLayout>
+#include <QList>
+#include <QPair>
+#include <QRegExp>
+#include <QHash>
+#include <QListWidgetItem>
+
+#include "SDLInteraction.h"
+
+#include "SmartLineEdit.h"
+
+class QTextBrowser;
+class QLineEdit;
+class QListView;
+class QSettings;
+class QAbstractItemModel;
+class QMenu;
+
+/**
+ * @brief Chat widget.
+ *
+ * By default uses :res/css/chat.css as style sheet for chat.
+ * See \repo{res/css/chat.css} for a more detailed description.
+ *
+ * @see http://doc.qt.nokia.com/4.5/richtext-html-subset.html#css-properties
+ */
+
+class HWChatWidget : public QWidget
+{
+        Q_OBJECT
+
+    public:
+        HWChatWidget(QWidget* parent, bool notify);
+        void setIgnoreListKick(bool enabled); ///< automatically kick people on ignore list (if possible)
+        void setShowFollow(bool enabled);
+        static const QString & styleSheet();
+        void displayError(const QString & message);
+        void displayNotice(const QString & message);
+        void displayWarning(const QString & message);
+        void setUser(const QString & nickname);
+        void setUsersModel(QAbstractItemModel * model);
+        void setSettings(QSettings * settings);
+
+    protected:
+        virtual void dragEnterEvent(QDragEnterEvent * event);
+        virtual void dropEvent(QDropEvent * event);
+
+    private:
+        static QString * s_styleSheet;
+        static QStringList * s_displayNone;
+        static bool s_isTimeStamped;
+        static QString s_tsFormat;
+        static const QRegExp URLREGEXP;
+
+        static void setStyleSheet(const QString & styleSheet = "");
+
+        void addLine(const QString & cssClass, QString line, bool isHighlight = false);
+        bool parseCommand(const QString & line);
+        void discardStyleSheet();
+        void saveStyleSheet();
+        QString linkedNick(const QString & nickname);
+        void beforeContentAdd();
+        void afterContentAdd();
+
+    public slots:
+        void onChatString(const QString& str);
+        void onChatString(const QString& nick, const QString& str);
+        void onServerMessage(const QString& str);
+        void nickAdded(const QString& nick, bool notifyNick);
+        void nickRemoved(const QString& nick);
+        void clear();
+        void adminAccess(bool);
+
+    signals:
+        void chatLine(const QString& str);
+        void kick(const QString & str);
+        void ban(const QString & str);
+        void info(const QString & str);
+        void follow(const QString &);
+        void nickCountUpdate(int cnt);
+        void consoleCommand(const QString & command);
+
+    private:
+        bool m_isAdmin;
+        QHBoxLayout mainLayout;
+        QTextBrowser* chatText;
+        QStringList chatStrings;
+        QListView* chatNicks;
+        SmartLineEdit* chatEditLine;
+        QAction * acInfo;
+        QAction * acKick;
+        QAction * acBan;
+        QAction * acFollow;
+        QAction * acIgnore;
+        QAction * acFriend;
+        QSettings * gameSettings;
+        QMenu * m_nicksMenu;
+        QStringList m_helloSounds;
+        QString m_hilightSound;
+        QString m_userNick;
+        QString m_clickedNick;
+        QList<QRegExp> m_highlights; ///< regular expressions used for highlighting
+        bool notify;
+        bool m_autoKickEnabled;
+        bool m_scrollToBottom;
+        int m_scrollBarPos;
+
+    private slots:
+        void returnPressed();
+        void onBan();
+        void onKick();
+        void onInfo();
+        void onFollow();
+        void onIgnore();
+        void onFriend();
+        void chatNickDoubleClicked(const QModelIndex & index);
+        void linkClicked(const QUrl & link);
+        void nicksContextMenuRequested(const QPoint & pos);
+};
+
+#endif // _CHAT_WIDGET_INCLUDED
