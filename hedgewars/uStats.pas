d3a1fe04ebb4022c20f10c117b425c11a97a9295
unc0rr <unc0rr@localhost>
2013-08-24 01:37:20 +0400

    Fix script flaw in detection of best team when sending achievement info

--- hedgewars/uStats.pas
+++ hedgewars/uStats.pas
@@ -309,8 +309,8 @@ ScriptCall('onAchievementsDeclaration');
 end;
 
 procedure declareAchievement(id, teamname, location: shortstring; value: LongInt);
-begin
-    if (length(id) = 0) or (length(teamname) = 0) then exit;
+begin 
+if (length(id) = 0) or (length(teamname) = 0) or (length(location) = 0) then exit;
     WriteLnToConsole('ACHIEVEMENT');
     WriteLnToConsole(id);
     WriteLnToConsole(teamname);
