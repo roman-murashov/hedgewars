55653e0b622e395901747ad095b955838c8af9b0
unc0rr <unc0rr@localhost>
2013-11-04 01:23:20 +0400

    Only toggle afk mode when it's not your turn

---
+++ QTfrontend/ui/page/pagedata.h
@@ -0,0 +1,61 @@
+/*
+ * Hedgewars, a free turn based strategy game
+ * Copyright (c) 2004-2013 Andrey Korotaev <unC0Rr@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
+ */
+
+#ifndef PAGE_DATA_H
+#define PAGE_DATA_H
+
+#include <QUrl>
+#include "AbstractPage.h"
+
+class DataBrowser;
+class QProgressBar;
+class QNetworkReply;
+class QVBoxLayout;
+
+class PageDataDownload : public AbstractPage
+{
+        Q_OBJECT
+
+    public:
+        PageDataDownload(QWidget* parent = 0);
+
+    public slots:
+        void fetchList();
+
+    protected:
+        QLayout * bodyLayoutDefinition();
+        void connectSignals();
+
+    private:
+        DataBrowser *web;
+        QHash<QNetworkReply*, QProgressBar *> progressBars;
+        QVBoxLayout *progressBarsLayout;
+
+        bool m_contentDownloaded; ///< true if something was downloaded since last page leave
+
+    private slots:
+        void request(const QUrl &url);
+
+        void pageDownloaded();
+        void fileDownloaded();
+        void downloadProgress(qint64, qint64);
+
+        void onPageLeave();
+};
+
+#endif
